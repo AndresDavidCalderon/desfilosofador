47e6d8171168e9249d476adb0de73131bf1b2e84
thepromaper <lospdfnoseeditan@gmail.com>
2021-12-04 13:17:46 -0500

    blockbase

---
+++ blueprints/blocks/blockbase.gd
@@ -0,0 +1,18 @@
+extends Panel
+class_name blockbase,"res://blueprints/blockbase.png"
+var following:bool=false
+var offset:Vector2
+func _on_select_button_down():
+	following=true
+	offset=rect_position-get_global_mouse_position()
+
+func _on_select_button_up():
+	following=false
+
+func _process(_delta):
+	if following:
+		rect_position=get_global_mouse_position()+offset
+
+func _input(event):
+	if event.is_action_pressed("delete") and following:
+		queue_free()
