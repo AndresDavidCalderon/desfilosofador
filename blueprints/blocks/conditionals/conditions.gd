05162ccd9e03ce79fe45f590085b536056daae47
thepromaper <lospdfnoseeditan@gmail.com>
2022-01-05 22:03:34 -0500

    if

---
+++ blueprints/blocks/conditionals/conditions.gd
@@ -0,0 +1,9 @@
+extends HBoxContainer
+
+
+func _on_add_pressed():
+	pass # Replace with function body.
+
+
+func _on_conditions_resized():
+	pass # Replace with function body.
--- blueprints/editor/add.gd
+++ blueprints/editor/add.gd
@@ -1,6 +1,10 @@
 extends HBoxContainer
 onready var workspace=get_parent().get_node("view/workspace")
 
-
-func _on_follows_pressed():
+func _on_follow_pressed():
 	workspace.addblock(preload("res://blueprints/blocks/conditionals/follows.tscn").instance())
+
+
+
+func _on_equals_pressed():
+	workspace.addblock(preload("res://blueprints/blocks/conditionals/equals.tscn").instance())
