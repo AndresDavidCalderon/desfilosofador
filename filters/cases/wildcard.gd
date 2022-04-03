90e6142980e9064ea9fca9299f62b305b3176dcc
thepromaper <lospdfnoseeditan@gmail.com>
2022-04-02 20:41:06 -0500

    wildcard fix

--- filters/cases/wildcard.gd
+++ filters/cases/wildcard.gd
@@ -2,13 +2,17 @@ extends Node
 
 
 static func found(_word,context:Dictionary):
-	if context["frontpos"]==0 and context["direction"]==-1:
+	var endpos=context["cut_until"]+context["next_on_filter"].length()
+	
+	if context["cut_until"]==0 and context["direction"]==-1:
 		return false
-	if context["frontpos"]==context["text"].length() and context["direction"]==1:
+	
+	var preceeds=context["text"][context["cut_until"]+context["direction"]]
+	if compiler.phrase_endings.has(preceeds) and context["direction"]==-1:
+		return false
+	
+	if (endpos==context["text"].length() or compiler.phrase_endings.has(context["text"][endpos])) and context["direction"]==1:
 		return false
 	#if the text ends there, its false
 	
-	var preceeds=context["text"][context["frontpos"]]+context["direction"]
-	if compiler.phrase_endings.has(preceeds):
-		return false
 	return true
